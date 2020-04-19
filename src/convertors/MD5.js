6972feaf3684292e8602285c202b5095f8da2048
Shankhanil Ghosh <shankha.rik@gmail.com>
2020-04-19 23:57:22 +0530

    fixing errors

--- src/convertors/MD5.js
+++ src/convertors/MD5.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /**
- *  File: _MD5.js
+ *  File: MD5.js
  *  Author: Shankhanil <shankha.rik@gmail.com>
  *  Description:  Encodes String --> MD5
  *  MD5 is a one way encoding algorithm,
@@ -10,12 +10,10 @@
  */
 
 define((require, exports) => {
-    
   const crypto = require('crypto-js');
-    
   const encodeToMD5 = (text) =>{
-      const hash = crypto.MD5(text).toString();
-      return hash;
+    const hash = crypto.MD5(text).toString();
+    return hash;
   };
   exports.encodeToMD5 = encodeToMD5;
 });
