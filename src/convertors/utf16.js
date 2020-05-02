cb8f5cd07abab02ab92745db85772dd6cc86aa23
nadchif <nadchif@gmail.com>
2020-05-02 21:32:45 +0200

    fixed lint errors

--- src/convertors/utf16.js
+++ src/convertors/utf16.js
@@ -6,16 +6,16 @@
  *  Description:  Encodes String --> UTF-16
  */
 
-define(function (require, exports) {
-    const encodeToUtf16 = (text) => {
-        // encode the provided string. function must return a string;
-        let encoded = "";
-        for (let i = 0; i < text.length; i++) {
-            encoded += text.codePointAt(i);
-        }
-        return encoded; // result must be a String
-    };
+define(function(require, exports) {
+  const encodeToUtf16 = (text) => {
+    // encode the provided string. function must return a string;
+    let encoded = '';
+    for (let i = 0; i < text.length; i++) {
+      encoded += text.codePointAt(i);
+    }
+    return encoded; // result must be a String
+  };
 
-    // export the encoder for use in the main module
-    exports.encodeToUtf16 = encodeToUtf16;
+  // export the encoder for use in the main module
+  exports.encodeToUtf16 = encodeToUtf16;
 });
