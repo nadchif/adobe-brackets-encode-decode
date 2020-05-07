1878ddd6175d21e29c99668c5f54b4e85c2d243a
Okan DAVUT <davut.okan@gmail.com>
2020-05-07 23:09:46 +0300

    Eslint problems solved #24

--- src/convertors/morsecode.js
+++ src/convertors/morsecode.js
@@ -1,46 +1,46 @@
 
 'use strict';
 
-    /**
-	*  File: morsecode.js
-	*  Author: Okan DAVUT <davut.okan@email.com>
-	*  Description:  Encodes and decodes String <--> Morse Code
-    */
+/**
+*  File: morsecode.js
+*  Author: Okan DAVUT <davut.okan@email.com>
+*  Description:  Encodes and decodes String <--> Morse Code
+  */
 
-define(function (require, exports) {
+define(function(require, exports) {
+  const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', '\n'];
+  const symbols = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---',
+    '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-',
+    '.--', '-..-', '-.--', '--..', '-----', '.----', '..---', '...--', '....-', '.....',
+    '-....', '--...', '---..', '----.', '^', '\n'];
 
-    var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", "\n"];
-    var symbols = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "^", "\n"];
-
-    // encode the provided string. function must return a string;
-    const encodeToMorseCode = (text) => {
-
-        var encResult = "";
-        var txt = text.trim().toUpperCase().split(""),
-            code = "";
-        for (var i in txt) {
-            code += symbols[letters.indexOf(txt[i])] + " ";
-        }
-        return code;
+  // encode the provided string. function must return a string;
+  const encodeToMorseCode = (text) => {
+    const txt = text.trim().toUpperCase().split('');
+    let code = '';
+    for (let i = 0; i < txt.length; i++) {
+      code += symbols[letters.indexOf(txt[i])] + ' ';
     }
-    // decode the provided string. function must return a string;
-    var decodeFromMorseCode = function (morseCode) {
-        var decoded = [];
-        var decResult = "";
+    return code;
+  };
+  // decode the provided string. function must return a string;
+  const decodeFromMorseCode = function(morseCode) {
+    let decResult = '';
 
-        var code = morseCode.trim().replace(/_|¯|—|–/g, "-").split(" "),
-            txt = "";
+    const code = morseCode.trim().replace(/_|¯|—|–/g, '-').split(' ');
+    let txt = '';
 
-        for (var i in code) {
-            txt += letters[symbols.indexOf(code[i])];
-        }
-        decResult = txt.replace("undefined", "");
-
-        return decResult.toLowerCase();
+    for (let i = 0; i < code.length; i++) {
+      txt += letters[symbols.indexOf(code[i])];
     }
+    decResult = txt.replace('undefined', '');
 
-    // export the encoder for use in the main module
-    exports.encodeToMorseCode = encodeToMorseCode;
-    // export the decoder for use in the main module
-    exports.decodeFromMorseCode = decodeFromMorseCode;
+    return decResult.toLowerCase();
+  };
+
+  // export the encoder for use in the main module
+  exports.encodeToMorseCode = encodeToMorseCode;
+  // export the decoder for use in the main module
+  exports.decodeFromMorseCode = decodeFromMorseCode;
 });
