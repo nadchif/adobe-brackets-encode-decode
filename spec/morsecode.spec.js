beb415d344d9751cf6b3962071c87f3f406d9964
Shankhanil Ghosh <shankha.rik@gmail.com>
2020-05-10 22:01:30 +0530

    writing test for morsecode, php-serial, MD5, SHA256, NLtoBR

--- spec/morsecode.spec.js
+++ spec/morsecode.spec.js
@@ -12,5 +12,34 @@ define(function(require, exports, module) {
   describe('src/morsecode.js =>', ()=>{
     const encodeToMorseCode = require('../src/convertors/morsecode').encodeToMorseCode;
     const decodeFromMorseCode = require('../src/convertors/morsecode').decodeFromMorseCode;
+
+    it('Should handle pure text', () =>{
+      const clearText = 'thisISaPUREtext';
+      const encodedText = '- .... .. ... .. ... .- .--. ..- .-. . - . -..- -';
+      expect(encodeToMorseCode(clearText)) == encodedText;
+      expect(decodeFromMorseCode(encodedText)) == clearText;
+    });
+    it('Should handle text with spaces', () =>{
+      const clearText = 'this is a text with spaces';
+      const encodedText = '- .... .. ...     .. ...     .-     - . -..- -     .-- .. - ....     ... .--. .- -.-. . ...';
+      expect(encodeToMorseCode(clearText)) == encodedText;
+      expect(decodeFromMorseCode(encodedText)) == clearText;
+    });
+
+    it('Should handle text with numbers', () =>{
+      const clearText = 'this IS a text with 21455';
+      const encodedText = '- .... .. ...     .. ...     .-     - . -..- -     .-- .. - ....     ..--- .---- ....- ..... .....';
+      expect(encodeToMorseCode(clearText)) == encodedText;
+      expect(decodeFromMorseCode(encodedText)) == clearText;
+    });
+
+    it('Should handle text with unsupported characters', () =>{
+      const clearText = 'this,IS:a;text-with_special+chars';
+      // eslint-disable-next-line max-len
+      const encodedText = '- .... .. ... --··-- .. ... ---··· .- -·-·-· - . -..- - -····- .-- .. - .... ··--·- ... .--. . -.-. .. .- .-.. ·-·-· -.-. .... .- .-. ...';
+      expect(encodeToMorseCode(clearText)) == encodedText;
+      expect(decodeFromMorseCode(encodedText)) == clearText;
+    });
+    // MORE TESTS TO HANDLE INVALID CHARACTERS
   });
 });
