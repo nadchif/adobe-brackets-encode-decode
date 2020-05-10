9dc194185491c1e60af00db47049cbca9929f776
Dan Chif <nadchif@gmail.com>
2020-05-10 20:12:10 +0200

    Updated Tests (#36)
    Co-authored-by: Shankhanil Ghosh <shankha.rik@gmail.com>

--- spec/SHA256.spec.js
+++ spec/SHA256.spec.js
@@ -8,7 +8,33 @@ require('amd-loader');
 define(function(require, exports, module) {
   'use strict';
   describe('src/SHA256.js =>', ()=>{
-    const encodeToMD5 = require('../convertors/MD5').encodeToMD5;
     const encodeToSHA256 = require('../convertors/SHA256').encodeToSHA256;
+    it('Should encode and decode pure text', () => {
+      const clearText = 'thisISaPUREtext';
+      // eslint-disable-next-line max-len
+      const encodedText = 'eff2844c77ff2de356e1c16b2ec9b6e238525b1fca3b3c20e7c0b6e41376596b';
+      expect(encodeToSHA256(clearText)) == encodedText;
+    });
+    it('Should encode and decode text with numbers', () => {
+      const clearText = 'thisISaPUREtextWITH1225';
+      // eslint-disable-next-line max-len
+      const encodedText = '1cc9943238e13d891948ccdca970f46519b70ce570a6328a619fb34ef44b3474';
+      expect(encodeToSHA256(clearText)) == encodedText;
+    });
+    describe('Encode/decode testing special characters =>', () => {
+      it('should encode and decode text with spaces', () =>{
+        const clearText = 'this is a text containing spaces';
+        // eslint-disable-next-line max-len
+        const encodeText = '021b802593087603fc94881c6529dea4da077307b5043a7f03e068f3cbae1edf';
+        expect(encodeToSHA256(clearText)) == encodeText;
+      });
+      it('Should encode and decode text with special characters', ()=>{
+        // NEED REVIEW
+        const clearText = 'TEXTwithSPECIALchars\'\"\\\/=!@#$%^&*(){}[];';
+        // eslint-disable-next-line max-len
+        const encodeText = '685b50ef1e499b48106705e14f26a023eb8c2684442220974ebe8e433a35e43f';
+        expect(encodeToSHA256(clearText)) == encodeText;
+      });
+    });
   });
 });
--- spec/hexadecimal.spec.js
+++ spec/hexadecimal.spec.js
@@ -10,5 +10,37 @@ define(function(require, exports, module) {
   describe('src/hexadecimal.js =>', ()=>{
     const encodeToHex = require('../src/convertors/hexadecimal').encodeToHex;
     const decodeFromHex = require('../src/convertors/hexadecimal').decodeFromHex;
+
+    it('Should encode and decode pure text', () => {
+      const clearText = 'thisISaPUREtext';
+      // eslint-disable-next-line max-len
+      const encodedText = '746869734953615055524574657874a';
+      expect(encodeToHex(clearText)) == encodedText;
+      expect(decodeFromHex(encodedText)) == clearText;
+    });
+    it('Should encode and decode text with numbers', () => {
+      const clearText = 'thisISaPUREtextWITH1225';
+      // eslint-disable-next-line max-len
+      const encodedText = '7468697349536150555245746578745749544831323235';
+      expect(encodeToHex(clearText)) == encodedText;
+      expect(decodeFromHex(encodedText)) == clearText;
+    });
+    describe('Encode/decode testing special characters =>', () => {
+      it('should encode and decode text with spaces', () =>{
+        const clearText = 'this is a text containing spaces';
+        // eslint-disable-next-line max-len
+        const encodeText = '746869732069732061207465787420636f6e7461696e696e6720737061636573';
+        expect(encodeToHex(clearText)) == clearText;
+        expect(decodeFromHex(encodeText)) == clearText;
+      });
+      it('Should encode and decode text with special characters', ()=>{
+        // NEED REVIEW
+        const clearText = '\'\"\\\/=!@#$%^&*(){}[];';
+        // eslint-disable-next-line max-len
+        const encodeText = '275c225c5c5c2f3d21402324255e262a28297b7d5b5d3b';
+        expect(encodeToHex(clearText)) == encodeText;
+        expect(decodeFromHex(encodeText)) == clearText;
+      });
+    });
   });
 });
