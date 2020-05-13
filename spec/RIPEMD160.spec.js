a7fd7cba2dc54a83fa680ffe5763b53a34290969
Arthur Pons <unguestdev@gmail.com>
2020-05-13 12:00:03 +0200

    Fixed issues with .spec file & added a Windows file to rebuild project easily

--- spec/RIPEMD160.spec.js
+++ spec/RIPEMD160.spec.js
@@ -7,33 +7,33 @@ require('amd-loader');
 // for tests that need RequireJS
 define(function(require, exports, module) {
   'use strict';
-  describe('src/SHA512.js =>', ()=>{
-    const encodeToSHA512 = require('../src/convertors/SHA512').encodeToSHA512;
+  describe('src/RIPEMD160.js =>', ()=>{
+    const encodeToRIPEMD160 = require('../src/convertors/RIPEMD160').encodeToRIPEMD160;
     it('Should encode and decode pure text', () => {
       const clearText = 'javascript';
       // eslint-disable-next-line max-len
       const encodedText = 'c10b83bcea4ab21102611a10ff9add87e2b3e7be';
-      expect(encodeToSHA512(clearText)) == encodedText;
+      expect(encodeToRIPEMD160(clearText)) == encodedText;
     });
     it('Should encode and decode text with numbers', () => {
       const clearText = 'Python3';
       // eslint-disable-next-line max-len
       const encodedText = '447da4169eed534a51f2c8a5c1d2d88eabcd81df';
-      expect(encodeToSHA512(clearText)) == encodedText;
+      expect(encodeToRIPEMD160(clearText)) == encodedText;
     });
     describe('Encode/decode testing special characters =>', () => {
       it('should encode and decode text with spaces', () =>{
         const clearText = 'I am an HTML developer.';
         // eslint-disable-next-line max-len
         const encodeText = 'b623aa45cd96afa80d6b9522e747330ff32e450e';
-        expect(encodeToSHA512(clearText)) == encodeText;
+        expect(encodeToRIPEMD160(clearText)) == encodeText;
       });
       it('Should encode and decode text with special characters', ()=>{
         // NEED REVIEW
         const clearText = 'I am $ure buT I am tout à fait sure que c\'est un coup de Fantomas !'; // A very good french reference :p
         // eslint-disable-next-line max-len
         const encodeText = '08d94410de558a66a71cea97d69c356254ab3e3e';
-        expect(encodeToSHA512(clearText)) == encodeText;
+        expect(encodeToRIPEMD160(clearText)) == encodeText;
       });
     });
   });
--- spec/SHA1.spec.js
+++ spec/SHA1.spec.js
@@ -7,33 +7,33 @@ require('amd-loader');
 // for tests that need RequireJS
 define(function(require, exports, module) {
   'use strict';
-  describe('src/SHA512.js =>', ()=>{
-    const encodeToSHA512 = require('../src/convertors/SHA512').encodeToSHA512;
+  describe('src/SHA1.js =>', ()=>{
+    const encodeToSHA1 = require('../src/convertors/SHA1').encodeToSHA1;
     it('Should encode and decode pure text', () => {
       const clearText = 'javascript';
       // eslint-disable-next-line max-len
       const encodedText = 'b6e13ad53d8ec41b034c49f131c64e99cf25207a';
-      expect(encodeToSHA512(clearText)) == encodedText;
+      expect(encodeToSHA1(clearText)) == encodedText;
     });
     it('Should encode and decode text with numbers', () => {
       const clearText = 'Python3';
       // eslint-disable-next-line max-len
       const encodedText = '260cdb02eef94c3ec0c5d6feeb1243b43c8d1491';
-      expect(encodeToSHA512(clearText)) == encodedText;
+      expect(encodeToSHA1(clearText)) == encodedText;
     });
     describe('Encode/decode testing special characters =>', () => {
       it('should encode and decode text with spaces', () =>{
         const clearText = 'I am an HTML developer.';
         // eslint-disable-next-line max-len
         const encodeText = 'd8d2d4f4ccca7ba90a27a751e95a18251a570cef';
-        expect(encodeToSHA512(clearText)) == encodeText;
+        expect(encodeToSHA1(clearText)) == encodeText;
       });
       it('Should encode and decode text with special characters', ()=>{
         // NEED REVIEW
         const clearText = 'I am $ure buT I am tout à fait sure que c\'est un coup de Fantomas !'; // A very good french reference :p
         // eslint-disable-next-line max-len
         const encodeText = '52f4c74eaabca2bbaa1f21eed97574e161b19f3c';
-        expect(encodeToSHA512(clearText)) == encodeText;
+        expect(encodeToSHA1(clearText)) == encodeText;
       });
     });
   });
